f8a56a4b59f9c8781df52b343cd73060961bc487
MihajlovicSS <mihajlovicsinisa1456780@gmail.com>
2023-02-17 12:49:37 +0200

    Second version

--- script.js
+++ script.js
@@ -9,7 +9,5 @@ function pow (number, degree){
 function fibo (number){
     if(number === 0) return 0
     if(number === 1) return 1
-    if(number > 1){
-        return fibo(number-1) + fibo(number-2);
-    }
+    if(number > 1) return fibo(number-1) + fibo(number-2)
 }
